be82d2b76eaf8a51e222f1788003d0a3a2480db7
Jospinlevrai <lukogomukanire@gmail.com>
2026-06-27 02:08:36 +0300

    creation of js for search bar

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
-const searchInput = document.getElementById('searchInput');
+const searchInput = document.getElementById("searchInput");
 
 if (searchInput) {
-    searchInput.addEventListener('keyup ', function () {
-        let filter = searchInput.value.toLowerCase();
-        let products = document.querySelectorAll('.product-card');
+    searchInput.addEventListener("keyup", function () {
 
+        const filter = searchInput.value.toLowerCase();
+
+        const products = document.querySelectorAll(".col-md-3.product");
 
         products.forEach(function (product) {
-            let text= product.innerText.toLowerCase();
+
+            const text = product.innerText.toLowerCase();
+
             if (text.includes(filter)) {
-                product.style.display = 'block';
+                product.style.display = "block";
             } else {
-                product.style.display = 'none';
+                product.style.display = "none";
             }
 
         });
+
     });
 }
